add06705091a4a1fb8d1f55c4bcca44c4082584a
Kenny Bates <kennyleebates@outlook.com>
2021-10-18 22:58:04 -0400

    Update App.js
    Updated App.js with state variable and function to fetch tours data from url. Additionally, I added an if statement to handle loading.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,29 @@ import Tours from './Tours';
 const url = 'https://course-api.com/react-tours-project'; // url to api for tours project
 
 function App() {
+  const [loading, setLoading] = useState(true); // state value for loading
+  const [tours, setTours] = useState([]); // state value for tours set to empty array
+
+  // fetch data
+  const fetchTours = async () => {
+    setLoading(true);
+    const response = await fetch(url);
+    const tours = await fetch.json();
+  }
+  
+  if(loading){
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
   return (
-    <>
-    <h2> Tours Project </h2>
-    </>
+    <main>
+      <Tours />
+    </main>
   );
+  
 }
 
 export default App;
